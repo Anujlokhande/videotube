41fdc77a863abe05a2014d9a3e9d1d851227f735
Anuj <anujlokhande25@gmail.com>
2025-12-21 14:30:45 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 import connectDb from "./db/index.js";
-connectDb();
+import { app } from "./app.js";
+
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Listening On port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Failed To Connect Database ", err);
+  });
